674fe53e2abc853d23b09441262def111e193d80
tenchd <dtench@protonmail.ch>
2023-12-19 10:11:45 -0800

    added comment for extending to weighted inputs later

--- sparsifier/src/tests.rs
+++ sparsifier/src/tests.rs
@@ -11,6 +11,8 @@ mod tests {
         for row in &s.rows {
             if current_row < s.first_zero_row {
                 assert!(row.is_occupied, "row {} is zeroed but first_zero_row is set to {}.", current_row, s.first_zero_row);
+                //the below logic doesn't hold if we extend this to handle weighted input edges. then you could have an input edge with weight <1.0 which hasn't been sparsified yet, 
+                //or one that is still that small after being sampled and having its weight increased.
                 assert!(row.weight >= 1.0, "row {} is marked as nonzero but has weight {} (should be >= 1.0).", current_row, row.weight);
             }
             else {
