309e5ea3151c17a56d21e5f3319008bc54abdb5c
David Tench <dtench@dtench-l78.dhcp.lbl.gov>
2025-06-17 15:51:25 -0700

    testing v0

--- sparsifier/src/main.rs
+++ sparsifier/src/main.rs
@@ -58,23 +58,12 @@ fn main1() {
 }
 
 fn main(){
-    //for i in 0..20 {
-    //    println!("{}", hash_with_inputs(0,1,i));
-    //}
-    /*
-    for i in 0..20{
-        let mut input: Array2<f64> = Array2::zeros((10,10));
-        populate_matrix(&mut input, i);
-        //println!("{:?}", input);
-        println!("{}", input.sum());
-    }
-    // */
-    let m = 20;
-    let n = 20;
+    let n = 2000; // rows in og matrix = # vertices
+    let m = 40000; // cols in og matrix = # edges. this dim will be sketched away
 
-    // /* 
-    let values = Array2::random((m, n), Uniform::new(0., 1.));
-    let mask = Array2::random((m, n), Bernoulli::new(0.2).expect("bernoulli gen failed"));
+ // /* 
+    let values = Array2::random((n, m), Uniform::new(0., 1.));
+    let mask = Array2::random((n, m), Bernoulli::new(0.1).expect("bernoulli gen failed"));
     let mask_closure = |val: &bool| -> f64 {
         if *val {
             return 1.
@@ -83,47 +72,45 @@ fn main(){
     };
     let newmask = mask.map(mask_closure);
     let input = &values*&newmask;
-    // */
+ // */
     
-/*
-        let mut input: Array2<f64> = Array2::zeros((m,n));
-    input[[1,1]] = 0.5;
-    input[[0,1]] = 0.6;
-    input[[3,5]] = 0.75;
-    input[[5,2]] = 0.2;
+  /*
+        let mut input: Array2<f64> = Array2::zeros((n,m));
+    input[[0,0]] = 0.22;
+    input[[9,8]] = 0.91;
+    //input[[3,5]] = 0.75;
+    //input[[5,2]] = 0.2;
 
-*/
+// */
+
     let sparse_input : CsMat<f64> = CsMat::csc_from_dense(input.view(), 0.);
-    println!("{:?}", input);
+    //println!("{:?}", input);
     //println!("{:?}", sparse_input);
 
 
 
-    /*
-    let bleh = sparse_input.indptr().outer_inds(1);
-    println!("{:?}", sparse_input);
-    for i in bleh {
-        println!("{:?}", sparse_input.data()[i]);
-    }
-    */
+
     let seed: u64 = 1;
     let jl_factor: f64 = 1.5;
     let jl_dim = ((n as f64).log2() *jl_factor).ceil() as usize;
-    println!("{}", jl_dim);
+    println!("output matrix is {}x{}", n, jl_dim);
 
     let sparse_result1 = jl_sketch_sparse(&sparse_input, jl_factor, seed);
     let result2 = jl_sketch_naive(&input, jl_factor, seed);
-    println!("{:?}", result2);
-    let sparse_result2 : CsMat<f64> = CsMat::csc_from_dense(result2.view(), -10.);
-    assert_eq!(sparse_result1, sparse_result2);
+    //println!("{:?}", result2);
+    let sparse_result2 : CsMat<f64> = CsMat::csc_from_dense(result2.view(), -1000000000.);
+    //assert_eq!(sparse_result1, sparse_result2);
 
     let mut blocked_result: CsMat<f64> = CsMat::zero((jl_dim,n)).transpose_into();
-    jl_sketch_sparse_blocked(&sparse_input, &mut blocked_result, jl_dim, seed, 3, 3, false);
+    let block_rows: usize = 9;
+    let block_cols: usize = 9;
+    jl_sketch_sparse_blocked(&sparse_input, &mut blocked_result, jl_dim, seed, block_rows, block_cols, false);
     //println!("{:?}",blocked_result);
+    //let diff = &blocked_result - &sparse_result2;
+    //println!("{:?}", diff);
     assert_eq!(sparse_result1, blocked_result);
 
 
 
-    //try_row_populate(20);
     
     }
